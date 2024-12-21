5c979f294b57c160abb2baaffb75b642e1e31fd9
julie9 <12814617+julie9@users.noreply.github.com>
2024-12-21 17:34:26 -0500

    comments (openmp) and adding BFS with label capacity

--- pulp/0.2/init_nonrandom.cpp
+++ pulp/0.2/init_nonrandom.cpp
@@ -103,8 +103,8 @@ int* init_nonrandom(pulp_graph_t& g, int num_parts, int* parts)
           int out = outs[j];
           if (parts[out] == -1)
           {
-            parts[out] = part;  // assign 'out' to part of 'vert' (start point)
-            thread_queue[thread_queue_size++] = out; // add 'out' to thread queue
+            parts[out]                        = part;
+            thread_queue[thread_queue_size++] = out;
 
             if (thread_queue_size == THREAD_QUEUE_SIZE)
             {
@@ -121,9 +121,6 @@ int* init_nonrandom(pulp_graph_t& g, int num_parts, int* parts)
       }
       #pragma omp atomic capture
       thread_start = next_size += thread_queue_size;
-      // i.e.:
-      //     thread_start = next_size;
-      //     next_size    = next_size + thread_queue_size;
 
       thread_start -= thread_queue_size;
       for (int l = 0; l < thread_queue_size; ++l)
@@ -141,14 +138,13 @@ int* init_nonrandom(pulp_graph_t& g, int num_parts, int* parts)
           queue_size = next_size;
           next_size  = 0;
       }
-    } // end while loop over queue size
+    }
 
-    // assign any remaining vertices to random parts
     #pragma omp for
     for (int i = 0; i < num_verts; ++i)
       if (parts[i] < 0)
         parts[i] = (unsigned)xs1024star_next(&xs) % num_parts;
-  } // end parallel
+  }
 
   #if OUTPUT_STEP
     evaluate_quality(g, num_parts, parts);
@@ -171,8 +167,155 @@ int* init_nonrandom(pulp_graph_t& g, int num_parts, int* parts)
 ########  ##        ######     #### ##    ## ####    ##        ######  #### ######## ########
 */
 // multi-source bfs with random start points
-// constrain maximal size of part, randomly assign any remaining
+// constrain to approximately equal part sizes, randomly assign any remaining.
 int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
+{
+  int num_verts = g.n;
+
+  int* queue      = new int[num_verts*QUEUE_MULTIPLIER];
+  int* queue_next = new int[num_verts*QUEUE_MULTIPLIER];
+  // The use of a QUEUE_MULTIPLIER of 2 in the code is likely a heuristic choice
+  // to ensure that the queue has enough capacity to handle the vertices that
+  // need to be processed during the breadth-first search (BFS) without running
+  // out of space.
+  int* part_sizes = new int[num_parts];
+  int  queue_size = num_parts;
+  int  next_size  = 0;
+
+  int  max_part_size = num_verts / num_parts * 2; // 2x average part size
+
+  #pragma omp parallel
+  {
+    int thread_queue[ THREAD_QUEUE_SIZE ];
+    int thread_queue_size = 0;
+    int thread_start;
+
+    xs1024star_t xs;
+    xs1024star_seed((unsigned long)(seed + omp_get_thread_num()), &xs);
+
+    #pragma omp for
+    for (int i = 0; i < num_verts; ++i)
+      parts[i] = -1;
+
+    // Randomly assign start points to parts
+    #pragma omp single
+    {
+      for (int i = 0; i < num_parts; ++i)
+      {
+        int vert = (int)xs1024star_next(&xs) % num_verts;
+        while (parts[vert] != -1) {vert = xs1024star_next(&xs) % num_verts;}
+        parts[vert]   = i;
+        queue[i]      = vert;
+        part_sizes[i] = 1;
+      }
+    }
+
+    // BFS from start points
+    while (queue_size > 0)
+    {
+      #pragma omp for schedule(guided) nowait
+      for (int i = 0; i < queue_size; ++i)
+      {
+        int  vert       = queue[i];               // start point
+        int  part       = parts[vert];            // part of start point
+        long out_degree = out_degree(g, vert);    // out degree of start point
+        int* outs       = out_vertices(g, vert);  // out vertices of start point
+        for (long j = 0; j < out_degree; ++j)
+        {
+          int out = outs[j];
+          if (parts[out] == -1)
+          {
+            // If part size is less than max_part_size (x2 average size),
+            // assign it to the same part as the start point.
+            // Otherwise, assign it to a random part
+            if (part_sizes[part] < max_part_size)
+              parts[out] = part;
+            else
+              parts[out] = (int)((unsigned)xs1024star_next(&xs) % (unsigned)num_parts);
+
+            #pragma omp atomic
+            ++part_sizes[parts[out]]; // increment part size
+
+            // thread_queue is a local array within each thread, used to
+            // temporarily store vertices that need to be processed in the next
+            // iteration of the BFS. Each thread maintains its own thread_queue
+            // to avoid race conditions when adding vertices to the queue.
+            thread_queue[thread_queue_size++] = out;
+
+            // Once thread_queue reaches its maximum size (THREAD_QUEUE_SIZE),
+            // its contents are transferred to the global queue_next array.
+            if (thread_queue_size == THREAD_QUEUE_SIZE)
+            {
+              // Ensures that each thread gets a unique and non-overlapping
+              // segment of the queue_next array
+              #pragma omp atomic capture
+              thread_start = next_size += thread_queue_size;
+
+              thread_start -= thread_queue_size;
+              for (int l = 0; l < thread_queue_size; ++l)
+                queue_next[thread_start+l] = thread_queue[l];
+              thread_queue_size = 0;
+            }
+          } // end if part not assigned yet
+        } // end for loop over out_degree
+      } // end for loop over queue_size
+
+      // Transfer remaining vertices from thread_queue to queue_next
+      #pragma omp atomic capture
+      thread_start = next_size += thread_queue_size;
+
+      thread_start -= thread_queue_size;
+      for (int l = 0; l < thread_queue_size; ++l)
+        queue_next[thread_start+l] = thread_queue[l];
+      thread_queue_size = 0;
+
+      #pragma omp barrier
+
+      // Swap queue and queue_next pointers and sizes after each iteration of
+      // the BFS to process the next level
+      #pragma omp single
+      {
+          int* temp  = queue;
+          queue      = queue_next;
+          queue_next = temp;
+
+          queue_size = next_size;
+          next_size  = 0;
+      }
+    } // end while
+
+    // Assign any remaining vertices to random parts
+    #pragma omp for
+    for (int i = 0; i < num_verts; ++i)
+      if (parts[i] == -1)
+        parts[i] = (int)((unsigned)xs1024star_next(&xs) % (unsigned)num_parts);
+  } // end parallel
+
+  #if OUTPUT_STEP
+    evaluate_quality(g, num_parts, parts);
+  #endif
+
+  delete [] queue;
+  delete [] queue_next;
+  delete [] part_sizes;
+
+  return parts;
+}
+
+
+
+/*
+########  ########  ######      ######     ###    ########     ###     ######  #### ######## ##    ##    #### ##    ## #### ########
+##     ## ##       ##    ##    ##    ##   ## ##   ##     ##   ## ##   ##    ##  ##     ##     ##  ##      ##  ###   ##  ##     ##
+##     ## ##       ##          ##        ##   ##  ##     ##  ##   ##  ##        ##     ##      ####       ##  ####  ##  ##     ##
+########  ######    ######     ##       ##     ## ########  ##     ## ##        ##     ##       ##        ##  ## ## ##  ##     ##
+##     ## ##             ##    ##       ######### ##        ######### ##        ##     ##       ##        ##  ##  ####  ##     ##
+##     ## ##       ##    ##    ##    ## ##     ## ##        ##     ## ##    ##  ##     ##       ##        ##  ##   ###  ##     ##
+########  ##        ######      ######  ##     ## ##        ##     ##  ######  ####    ##       ##       #### ##    ## ####    ##
+*/
+// multi-source bfs with random start points
+// constrain to capacity, randomly assign any remaining.
+int* init_nonrandom_constrained_capacity(pulp_graph_t& g, int num_parts, int* parts)
 {
   int num_verts = g.n;
 
@@ -196,6 +339,7 @@ int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
     for (int i = 0; i < num_verts; ++i)
       parts[i] = -1;
 
+    // Randomly assign start points to parts
     #pragma omp single
     {
       for (int i = 0; i < num_parts; ++i)
@@ -208,6 +352,7 @@ int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
       }
     }
 
+    // BFS from start points
     while (queue_size > 0)
     {
       #pragma omp for schedule(guided) nowait
@@ -222,8 +367,9 @@ int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
           int out = outs[j];
           if (parts[out] == -1)
           {
-            // If part size is less than max_part_size, assign 'out' to part of 'vert'
-            // Otherwise, assign 'out' to a random part
+            // If part size is less than max_part_size (x2 average size),
+            // assign it to the same part as the start point.
+            // Otherwise, assign it to a random part
             if (part_sizes[part] < max_part_size)
               parts[out] = part;
             else
@@ -232,9 +378,18 @@ int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
             #pragma omp atomic
             ++part_sizes[parts[out]]; // increment part size
 
+            // thread_queue is a local array within each thread, used to
+            // temporarily store vertices that need to be processed in the
+            // next iteration of the BFS. Each thread maintains its own thread_queue
+            // to avoid race conditions when adding vertices to the queue.
             thread_queue[thread_queue_size++] = out;
+
+            // Once thread_queue reaches its maximum size (THREAD_QUEUE_SIZE),
+            // its contents are transferred to the global queue_next array.
             if (thread_queue_size == THREAD_QUEUE_SIZE)
             {
+              // Ensures that each thread gets a unique and non-overlapping
+              // segment of the queue_next array
               #pragma omp atomic capture
               thread_start = next_size += thread_queue_size;
 
@@ -284,4 +439,3 @@ int* init_nonrandom_constrained(pulp_graph_t& g, int num_parts, int* parts)
 
   return parts;
 }
-
